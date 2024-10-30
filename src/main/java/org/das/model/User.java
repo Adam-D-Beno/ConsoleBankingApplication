7debf5eeb31b8755eb0e20a8787562864a0ec2ed
ПК_Home <wrofdt@ya.ru>
2024-10-30 22:07:20 +0300

    refactoring user: add constructor

--- src/main/java/org/das/model/User.java
+++ src/main/java/org/das/model/User.java
@@ -2,14 +2,35 @@ package org.das.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Component
+
 public class User {
-    private UUID uuid;
+    private UUID userId;
     private String login;
     private List<Account> accounts;
 
 
+    public User() {
+        this.userId = UUID.randomUUID();
+        this.accounts = new ArrayList<>();
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Account account) {
+        this.accounts.add(account);
+    }
 }
