7dade1ff95a9529f1ed27dacfbd5b21f1f71b186
ПК_Home <wrofdt@ya.ru>
2024-10-31 00:07:57 +0300

    refactoring Account: add constructor

--- src/main/java/org/das/model/Account.java
+++ src/main/java/org/das/model/Account.java
@@ -11,4 +11,41 @@ public class Account {
         this.userId = userId;
         this.accountId = UUID.randomUUID();
     }
+
+    public UUID getAccountId() {
+        return accountId;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public double getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Account account = (Account) o;
+        return accountId.equals(account.accountId) && userId.equals(account.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = accountId.hashCode();
+        result = 31 * result + userId.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId=" + accountId +
+                ", userId=" + userId +
+                ", moneyAmount=" + moneyAmount +
+                '}';
+    }
 }
