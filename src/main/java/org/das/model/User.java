cbc502223fa9f01c8f9e260cabb83e3e36c27d46
ПК_Home <wrofdt@ya.ru>
2024-10-31 00:08:37 +0300

    refactoring User

--- src/main/java/org/das/model/User.java
+++ src/main/java/org/das/model/User.java
@@ -13,9 +13,10 @@ public class User {
     private List<Account> accounts;
 
 
-    public User() {
+    public User(String login) {
         this.userId = UUID.randomUUID();
         this.accounts = new ArrayList<>();
+        this.login = login;
     }
 
     public UUID getUserId() {
@@ -32,5 +33,30 @@ public class User {
 
     public void setAccounts(Account account) {
         this.accounts.add(account);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+        return userId.equals(user.userId) && login.equals(user.login);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId.hashCode();
+        result = 31 * result + login.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", login='" + login + '\'' +
+                ", accounts=" + accounts +
+                '}';
     }
 }
