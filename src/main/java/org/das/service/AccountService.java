769707c2b916a8404b97c8f5f78b2d4757804e48
ПК_Home <wrofdt@ya.ru>
2024-10-31 00:08:14 +0300

    refactoring AccountService: add constructor

--- src/main/java/org/das/service/AccountService.java
+++ src/main/java/org/das/service/AccountService.java
@@ -2,10 +2,12 @@ package org.das.service;
 
 import org.das.model.Account;
 
+import java.util.UUID;
+
 public interface AccountService {
-    Account accountCreate();
-    boolean accountClose();
-    boolean accountDeposit();
-    boolean accountTransfer();
-    boolean accountWithdraw();
+    Account accountCreate(UUID userId);
+    boolean accountClose(UUID accountId);
+    boolean accountDeposit(UUID accountId, double amount);
+    boolean accountTransfer(UUID senderId, UUID recipientId, double amount);
+    boolean accountWithdraw(UUID accountId, double amount);
 }
