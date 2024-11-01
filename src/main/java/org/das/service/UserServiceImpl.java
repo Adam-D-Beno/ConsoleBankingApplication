d08e26be50cd073fb3f14414d1b02c89dd298938
ПК_Home <wrofdt@ya.ru>
2024-11-01 16:20:31 +0300

    refactoring UserServiceImpl

--- src/main/java/org/das/service/UserServiceImpl.java
+++ src/main/java/org/das/service/UserServiceImpl.java
@@ -1,39 +1,37 @@
 package org.das.service;
 
+import org.das.dao.UserDao;
 import org.das.model.Account;
 import org.das.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class UserServiceImpl implements UserService {
-    private final Map<String, User> userMap;
+    private final UserDao users;
     private final AccountService accountService;
 
     @Autowired
-    public UserServiceImpl(AccountServiceImpl accountService) {
+    public UserServiceImpl(UserDao users, AccountServiceImpl accountService) {
+        this.users = users;
         this.accountService = accountService;
-        this.userMap = new HashMap<>();
 
     }
 
     @Override
     public User userCreate(String login) {
-        if (userMap.containsKey(login)) {
-            return userMap.get(login);
+        if (users.userExist(login)) {
+            return users.getUser(login);
         }
         User user = new User(login);
         Account account = accountService.accountCreate(user.getUserId());
         user.setAccounts(account);
-        userMap.put(user.getLogin(), user);
+        users.setUser(user);
         return user;
     }
 
     @Override
     public void showAllUsers() {
-        userMap.forEach((key, value) -> System.out.println(value));
+        users.getUsers().forEach((key, value) -> System.out.println(value));
     }
 }
