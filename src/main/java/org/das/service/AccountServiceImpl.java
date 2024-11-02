6bba078d023be688141778c69ff327d7a3574c1c
ПК_Home <wrofdt@ya.ru>
2024-11-02 09:26:24 +0300

    refactoring AccountServiceImpl.java

--- src/main/java/org/das/service/AccountServiceImpl.java
+++ src/main/java/org/das/service/AccountServiceImpl.java
@@ -1,11 +1,24 @@
 package org.das.service;
 
+import org.das.dao.UserDao;
 import org.das.model.Account;
+import org.das.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class AccountServiceImpl implements AccountService {
+    private final UserDao users;
+
+    @Autowired
+    public AccountServiceImpl(UserDao users) {
+        this.users = users;
+    }
+
+
     @Override
     public Account accountCreate(UUID userId) {
         return new Account(userId);
@@ -17,8 +30,11 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public boolean accountDeposit(UUID accountId, double amount) {
-        return false;
+    public void accountDeposit(UUID accountId, double amount) {
+        Account account = findAccountById(accountId)
+                .orElseThrow(() -> new RuntimeException("Account not exist"));;
+        account.increaseAmount(amount);
+
     }
 
     @Override
@@ -27,7 +43,21 @@ public class AccountServiceImpl implements AccountService {
     }
 
     @Override
-    public boolean accountWithdraw(UUID accountId, double amount) {
-        return false;
+    public void accountWithdraw(UUID accountId, double amount) {
+        Account account = findAccountById(accountId)
+                .orElseThrow(() -> new RuntimeException("Account not exist"));;
+        account.decreaseAmount(amount);
+    }
+
+
+    private Optional<Account> findAccountById(UUID accountId) {
+
+        for (User user : users.getUsers().values()) {
+            Optional<Account> account = user.getAccountById(accountId);
+            if (account.isPresent()) {
+               return account;
+            }
+        }
+        return Optional.empty();
     }
 }
