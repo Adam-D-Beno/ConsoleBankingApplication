509dc318f4c7a991a454f04e0010bc6c83b0f6fc
ПК_Home <wrofdt@ya.ru>
2024-11-02 09:26:18 +0300

    refactoring AccountService

--- src/main/java/org/das/service/AccountService.java
+++ src/main/java/org/das/service/AccountService.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public interface AccountService {
     Account accountCreate(UUID userId);
     boolean accountClose(UUID accountId);
-    boolean accountDeposit(UUID accountId, double amount);
+    void accountDeposit(UUID accountId, double amount);
     boolean accountTransfer(UUID senderId, UUID recipientId, double amount);
-    boolean accountWithdraw(UUID accountId, double amount);
+    void accountWithdraw(UUID accountId, double amount);
 }
