e1b3698cd41db29d456b809d6c93de5c3a62b5c5
ПК_Home <wrofdt@ya.ru>
2024-11-03 09:44:34 +0300

    refactor AccountValidation.java

---
+++ src/main/java/org/das/validate/AccountValidation.java
@@ -0,0 +1,24 @@
+package org.das.validate;
+
+import org.das.model.Account;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class AccountValidation {
+
+    public void negativeAmount(BigDecimal amount) {
+        if (amount.signum() == -1) {
+            throw new RuntimeException("Amount is negative: " + amount);
+        }
+    }
+
+    public void negativeBalance(Account account, BigDecimal amount) {
+        BigDecimal balance = account.getMoneyAmount();
+        if (balance.subtract(amount).signum() == -1) {
+            throw new RuntimeException("Insufficient balance: " + balance
+                    + " account id " + account.getAccountId());
+        }
+    }
+}
