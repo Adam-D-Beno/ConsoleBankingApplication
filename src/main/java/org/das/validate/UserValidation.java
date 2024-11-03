2427c9675f284dc1e11e7e2ed8a81be0df692961
ПК_Home <wrofdt@ya.ru>
2024-11-03 20:48:34 +0300

    refactor UserValidation.java

--- src/main/java/org/das/validate/UserValidation.java
+++ src/main/java/org/das/validate/UserValidation.java
@@ -1,8 +1,28 @@
 package org.das.validate;
 
+import org.apache.commons.lang3.StringUtils;
+import org.das.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserValidation {
+    private final UserDao userDao;
 
+    @Autowired
+    public UserValidation(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void userAlreadyExist(String login) {
+        if (userDao.userExist(login)) {
+            throw new RuntimeException("User with login: " + login + " already exist");
+        }
+    }
+
+    public void userLoginCorrect(String login) {
+       if (StringUtils.isBlank(login)) {
+           throw new RuntimeException("Login: " + login + " is NULL or Empty");
+       }
+    }
 }
