f98f8a91d99d1ea14dc6fd5dda0982491537c0a4
ПК_Home <wrofdt@ya.ru>
2024-11-03 20:48:23 +0300

    refactor UserServiceImpl.java

--- src/main/java/org/das/service/UserServiceImpl.java
+++ src/main/java/org/das/service/UserServiceImpl.java
@@ -9,30 +9,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private final UserDao users;
+    private final UserDao userDao;
     private final AccountService accountService;
     private final UserValidation userValidation;
 
     @Autowired
-    public UserServiceImpl(UserDao users, AccountServiceImpl accountService, UserValidation userValidation) {
-        this.users = users;
+    public UserServiceImpl(UserDao userDao, AccountServiceImpl accountService, UserValidation userValidation) {
+        this.userDao = userDao;
         this.accountService = accountService;
         this.userValidation = userValidation;
     }
 
     @Override
     public User userCreate(String login) {
-        if (users.userExist(login)) {
-            throw new RuntimeException("User with login: " + login + " all read exist");
-        }
+        userValidation.userLoginCorrect(login);
+        userValidation.userAlreadyExist(login);
         User user = new User(login);
         user.setAccounts(accountService.accountCreate(user.getUserId()));
-        users.saveUser(user);
+        userDao.saveUser(user);
         return user;
     }
 
     @Override
     public void showAllUsers() {
-        users.getUsers().forEach((key, value) -> System.out.println(value));
+        userDao.getUsers().forEach((key, value) -> System.out.println(value));
     }
 }
