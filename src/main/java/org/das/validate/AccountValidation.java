b46f825e64adb48c35b931b8aca0a0a229d9d84f
ПК_Home <wrofdt@ya.ru>
2024-11-03 20:47:23 +0300

    refactor AccountValidation.java

--- src/main/java/org/das/validate/AccountValidation.java
+++ src/main/java/org/das/validate/AccountValidation.java
@@ -1,12 +1,21 @@
 package org.das.validate;
 
+import org.das.dao.AccountDao;
 import org.das.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Component
 public class AccountValidation {
+    private final AccountDao accountDao;
+
+    @Autowired
+    public AccountValidation(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
 
     public void negativeAmount(BigDecimal amount) {
         if (amount.signum() == -1) {
@@ -19,6 +28,12 @@ public class AccountValidation {
         if (balance.subtract(amount).signum() == -1) {
             throw new RuntimeException("Insufficient balance: " + balance
                     + " account id " + account.getAccountId());
+        }
+    }
+
+    public void accountAlreadyExist(UUID id) {
+        if (!accountDao.AccountExist(id)) {
+            throw new RuntimeException("User with account id: " + id + " already exist");
         }
     }
 }
