c1bc2620f502e77196c5f09fd32ea8ee6822539f
NoutVivo <fst8822@gmail.com>
2024-11-04 21:05:03 +0300

    refactor UserService.java

--- src/main/java/org/das/service/UserService.java
+++ src/main/java/org/das/service/UserService.java
@@ -2,8 +2,12 @@ package org.das.service;
 
 import org.das.model.User;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserService {
    User userCreate(String login);
    void showAllUsers();
+   Optional<User> getUserByLogin(UUID id);
 
 }
