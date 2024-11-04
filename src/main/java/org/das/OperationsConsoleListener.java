a222eac9cc3be2ff34e15279f4a02177015bedd5
NoutVivo <fst8822@gmail.com>
2024-11-04 21:04:29 +0300

    refactor OperationsConsoleListener.java

--- src/main/java/org/das/OperationsConsoleListener.java
+++ src/main/java/org/das/OperationsConsoleListener.java
@@ -4,11 +4,13 @@ import org.das.model.Account;
 import org.das.model.User;
 import org.das.service.AccountService;
 import org.das.service.UserService;
-import org.das.utils.ExecuteUserInputs;
+import org.das.utils.ExecuteOperation;
 import org.das.validate.AccountValidation;
 import org.das.validate.UserValidation;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.util.Scanner;
+import java.util.UUID;
 
 public class OperationsConsoleListener {
     public static void main(String[] args) {
@@ -19,7 +21,7 @@ public class OperationsConsoleListener {
         UserService userService = context.getBean(UserService.class);
         AccountValidation accountValidation = context.getBean(AccountValidation.class);
         UserValidation userValidation = context.getBean(UserValidation.class);
-        ExecuteUserInputs executeUserInputs = context.getBean(ExecuteUserInputs.class);
+        ExecuteOperation executeOperation = context.getBean(ExecuteOperation.class);
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -34,15 +36,19 @@ public class OperationsConsoleListener {
                     -USER_CREATE""");
 
             String userEnter = scanner.nextLine();
-            if (userEnter.equals("ACCOUNT_CREATE")) {}
-            if (userEnter.equals("SHOW_ALL_USERS")) {}
+            if (userEnter.equals("ACCOUNT_CREATE")) {
+                executeOperation.executeOperationsAccountCreate(scanner);
+            }
+            if (userEnter.equals("SHOW_ALL_USERS")) {
+                executeOperation.showAllUsers();
+            }
             if (userEnter.equals("ACCOUNT_CLOSE")) {}
             if (userEnter.equals("ACCOUNT_WITHDRAW")) {}
-            if (userEnter.equals("ACCOUNT_DEPOSIT")) {}
+            if (userEnter.equals("ACCOUNT_DEPOSIT")) {
+                executeOperation.executeOperationsAccountWithdraw(scanner);
+            }
             if (userEnter.equals("USER_CREATE")) {
-                String login = executeUserInputs.executeOperationsUserCreate(scanner);
-                User user = userService.userCreate(login);
-                System.out.println("User created successfully " + user.toString());
+                executeOperation.executeOperationsUserCreate(scanner);
             }
         }
     }
