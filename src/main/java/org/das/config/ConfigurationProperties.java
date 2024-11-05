6ddcbd78c531296596a3442ccc0b56ae3b2fa7fb
NoutVivo <fst8822@gmail.com>
2024-11-05 10:50:00 +0300

    refactor ConfigurationProperties.java

--- src/main/java/org/das/config/ConfigurationProperties.java
+++ src/main/java/org/das/config/ConfigurationProperties.java
@@ -1,9 +1,10 @@
 package org.das.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @PropertySource("classpath:application.properties")
 public class ConfigurationProperties {
 
