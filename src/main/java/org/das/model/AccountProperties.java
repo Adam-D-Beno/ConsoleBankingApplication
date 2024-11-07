9cbfe6e94971c060c879f0f143088fe66d1e5684
NoutVivo <fst8822@gmail.com>
2024-11-07 22:13:27 +0300

    Create AccountProperties class

---
+++ src/main/java/org/das/model/AccountProperties.java
@@ -0,0 +1,24 @@
+package org.das.model;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountProperties {
+    private final int defaultAmount;
+    private final double transferComission;
+
+    public AccountProperties(@Value("${account.default-amount}") int defaultAmount,
+                             @Value("${account.transfer-commission}") double transferComission) {
+        this.defaultAmount = defaultAmount;
+        this.transferComission = transferComission;
+    }
+
+    public int getDefaultAmount() {
+        return defaultAmount;
+    }
+
+    public double getTransferComission() {
+        return transferComission;
+    }
+}
