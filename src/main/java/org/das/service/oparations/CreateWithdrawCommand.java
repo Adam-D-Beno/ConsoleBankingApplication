c1dfbba5e3b99d212e0f193061a97b6b991fec24
ПК_Home <wrofdt@ya.ru>
2024-11-12 23:00:27 +0300

    refactor CreateWithdrawCommand

--- src/main/java/org/das/service/oparations/CreateWithdrawCommand.java
+++ src/main/java/org/das/service/oparations/CreateWithdrawCommand.java
@@ -1,15 +1,40 @@
 package org.das.service.oparations;
 
+import org.das.service.AccountService;
 import org.das.utils.ConsoleOperationType;
+import org.das.validate.UserValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.UUID;
+
+@Component
 public class CreateWithdrawCommand implements OperationCommand {
+    private final UserValidation userValidation;
+    private final AccountService accountService;
+
+    @Autowired
+    public CreateWithdrawCommand(UserValidation userValidation, AccountService accountService) {
+        this.userValidation = userValidation;
+        this.accountService = accountService;
+    }
+
     @Override
     public void execute() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter account ID to withdraw from: ");
+        String accountId = scanner.nextLine();
+        userValidation.userLoginCorrect(accountId);
+        System.out.println("Enter amount to withdraw: ");
+        double amount = scanner.nextDouble();
+        accountService.withdraw(UUID.fromString(accountId), BigDecimal.valueOf(amount));
+        System.out.println("Amount " + amount + " withdraw to account ID: " + accountId);
     }
 
     @Override
     public ConsoleOperationType getOperationType() {
-        return null;
+        return ConsoleOperationType.ACCOUNT_WITHDRAW;
     }
 }
