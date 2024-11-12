c9d52d17d04a767acdb5c0ce59b0f219473169fa
ПК_Home <wrofdt@ya.ru>
2024-11-12 23:00:13 +0300

    refactor CreateTransferCommand

--- src/main/java/org/das/service/oparations/CreateTransferCommand.java
+++ src/main/java/org/das/service/oparations/CreateTransferCommand.java
@@ -1,15 +1,42 @@
 package org.das.service.oparations;
 
+import org.das.service.AccountService;
 import org.das.utils.ConsoleOperationType;
+import org.das.validate.UserValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.math.BigDecimal;
+import java.util.Scanner;
+import java.util.UUID;
 
+@Component
 public class CreateTransferCommand implements OperationCommand {
+    private final UserValidation userValidation;
+    private final AccountService accountService;
+
+    @Autowired
+    public CreateTransferCommand(UserValidation userValidation, AccountService accountService) {
+        this.userValidation = userValidation;
+        this.accountService = accountService;
+    }
+
     @Override
     public void execute() {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter source account ID: ");
+        String source = scanner.nextLine();
+        userValidation.userLoginCorrect(source);
+        System.out.println("Enter target account ID: ");
+        String target = scanner.nextLine();
+        userValidation.userLoginCorrect(target);
+        System.out.println("Enter amount to transfer: ");
+        double amount = scanner.nextDouble();
+        accountService.transfer(UUID.fromString(source), UUID.fromString(target), BigDecimal.valueOf(amount));
+        System.out.println(" Amount " + amount + " transferred from account ID " + source + " to account ID " + target);
     }
 
     @Override
     public ConsoleOperationType getOperationType() {
-        return null;
+        return ConsoleOperationType.ACCOUNT_TRANSFER;
     }
 }
