e4aad12f4c4da6512a7fc6e257b7a867039315a7
ПК_Home <wrofdt@ya.ru>
2024-11-12 23:00:20 +0300

    refactor CreateUserCommand

--- src/main/java/org/das/service/oparations/CreateUserCommand.java
+++ src/main/java/org/das/service/oparations/CreateUserCommand.java
@@ -1,15 +1,37 @@
 package org.das.service.oparations;
 
+import org.das.model.User;
+import org.das.service.UserService;
 import org.das.utils.ConsoleOperationType;
+import org.das.validate.UserValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
+@Component
 public class CreateUserCommand implements OperationCommand {
+    private final UserValidation userValidation;
+    private final UserService userService;
+
+    @Autowired
+    public CreateUserCommand(UserValidation userValidation, UserService userService) {
+        this.userValidation = userValidation;
+        this.userService = userService;
+    }
 
     @Override
     public void execute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the login for new user ");
+        String login = scanner.nextLine();
+        userValidation.userLoginCorrect(login);
+        User user = userService.create(login);
+        System.out.println("User created successfully " + user.toString());
     }
 
     @Override
     public ConsoleOperationType getOperationType() {
-        return null;
+        return ConsoleOperationType.USER_CREATE;
     }
 }
