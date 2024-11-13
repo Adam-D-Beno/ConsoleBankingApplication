2a26fec72d6a7c32220c86a747f2f2d3f990cebc
ПК_Home <wrofdt@ya.ru>
2024-11-13 21:57:24 +0300

    refactor Main

--- src/main/java/org/das/Main.java
+++ src/main/java/org/das/Main.java
@@ -1,5 +1,6 @@
 package org.das;
 
+import org.das.service.oparations.OperationsConsoleListener;
 import org.das.utils.ExecuteOperation;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,42 +11,7 @@ public class Main {
 
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext("org.das");
-        ExecuteOperation executeOperation = context.getBean(ExecuteOperation.class);
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("""
-                     Please enter one of operation type:
-                    -ACCOUNT_CREATE
-                    -SHOW_ALL_USERS
-                    -ACCOUNT_CLOSE
-                    -ACCOUNT_WITHDRAW
-                    -ACCOUNT_DEPOSIT
-                    -ACCOUNT_TRANSFER
-                    -USER_CREATE""");
-
-            String userEnter = scanner.nextLine();
-            if (userEnter.equals("ACCOUNT_CREATE")) {
-                executeOperation.executeOperationsAccountCreate(scanner);
-            }
-            if (userEnter.equals("SHOW_ALL_USERS")) {
-                executeOperation.showAllUsers();
-            }
-            if (userEnter.equals("ACCOUNT_CLOSE")) {
-                executeOperation.executeOperationsAccountClose(scanner);
-            }
-            if (userEnter.equals("ACCOUNT_WITHDRAW")) {
-                executeOperation.executeOperationsAccountWithdraw(scanner);
-            }
-            if (userEnter.equals("ACCOUNT_DEPOSIT")) {
-                executeOperation.executeOperationsAccountDeposit(scanner);
-            }
-            if (userEnter.equals("USER_CREATE")) {
-                executeOperation.executeOperationsUserCreate(scanner);
-            }
-            if (userEnter.equals("ACCOUNT_TRANSFER")) {
-                executeOperation.executeOperationsAccountTransfer(scanner);
-            }
-        }
+        OperationsConsoleListener operationsConsoleListener = context.getBean(OperationsConsoleListener.class);
+        operationsConsoleListener.run();
     }
 }
