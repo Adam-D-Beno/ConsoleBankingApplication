07a387b6dc549e1e9a81bad00d6f3676d8edde93
ПК_Home <wrofdt@ya.ru>
2024-11-15 22:36:34 +0300

    refactor App

--- src/main/java/org/das/App.java
+++ src/main/java/org/das/App.java
@@ -6,7 +6,6 @@ public class App {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext("org.das");
-
         context.getBean(OperationsConsoleListener.class).listenUpdates();
     }
 }
