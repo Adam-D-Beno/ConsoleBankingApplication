35e70bae850c62b986473198ee22fe2a6fe243e4
ПК_Home <wrofdt@ya.ru>
2024-11-15 22:34:45 +0300

    create App class

---
+++ src/main/java/org/das/App.java
@@ -0,0 +1,12 @@
+package org.das;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext("org.das");
+
+        context.getBean(OperationsConsoleListener.class).listenUpdates();
+    }
+}
