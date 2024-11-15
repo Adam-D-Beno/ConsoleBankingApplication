fcc86a516548091f00ca71fee004cab0b3dcefa0
ПК_Home <wrofdt@ya.ru>
2024-11-15 22:34:31 +0300

    refactor OperationsConsoleListener

--- src/main/java/org/das/OperationsConsoleListener.java
+++ src/main/java/org/das/OperationsConsoleListener.java
@@ -1,19 +1,36 @@
 package org.das;
 
 import org.das.utils.ExecuteOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
-
+@Component
 public class OperationsConsoleListener {
-    public static void main(String[] args) {
+    private final Scanner scanner;
+    private final ExecuteOperation executeOperation;
 
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext("org.das");
-        ExecuteOperation executeOperation = context.getBean(ExecuteOperation.class);
-        Scanner scanner = context.getBean(Scanner.class);
+    @Autowired
+    public OperationsConsoleListener(Scanner scanner, ExecuteOperation executeOperation) {
+        this.scanner = scanner;
+        this.executeOperation = executeOperation;
+    }
+
+    public void listenUpdates() {
 
         while (true) {
-            System.out.println("""
+            String operationType = listenNextOperation();
+            try {
+                processNextOperation(operationType);
+            } catch (Exception e) {
+                System.out.printf("Error executing command %s: error=%s%n", operationType, e.getMessage());
+            }
+        }
+    }
+
+    private String listenNextOperation() {
+        System.out.println("""
                      Please enter one of operation type:
                     -ACCOUNT_CREATE
                     -SHOW_ALL_USERS
@@ -23,28 +40,31 @@ public class OperationsConsoleListener {
                     -ACCOUNT_TRANSFER
                     -USER_CREATE""");
 
-            String userEnter = scanner.nextLine();
-            if (userEnter.equals("ACCOUNT_CREATE")) {
-                executeOperation.executeOperationsAccountCreate(scanner);
-            }
-            if (userEnter.equals("SHOW_ALL_USERS")) {
-                executeOperation.showAllUsers();
-            }
-            if (userEnter.equals("ACCOUNT_CLOSE")) {
-                executeOperation.executeOperationsAccountClose(scanner);
-            }
-            if (userEnter.equals("ACCOUNT_WITHDRAW")) {
-                executeOperation.executeOperationsAccountWithdraw(scanner);
-            }
-            if (userEnter.equals("ACCOUNT_DEPOSIT")) {
-                executeOperation.executeOperationsAccountDeposit(scanner);
-            }
-            if (userEnter.equals("USER_CREATE")) {
-                executeOperation.executeOperationsUserCreate(scanner);
-            }
-            if (userEnter.equals("ACCOUNT_TRANSFER")) {
-                executeOperation.executeOperationsAccountTransfer(scanner);
-            }
+        return scanner.nextLine();
+    }
+
+    private void processNextOperation(String operation) {
+
+        if (operation.equals("ACCOUNT_CREATE")) {
+            executeOperation.executeOperationsAccountCreate(scanner);
+        }
+        if (operation.equals("SHOW_ALL_USERS")) {
+            executeOperation.showAllUsers();
+        }
+        if (operation.equals("ACCOUNT_CLOSE")) {
+            executeOperation.executeOperationsAccountClose(scanner);
+        }
+        if (operation.equals("ACCOUNT_WITHDRAW")) {
+            executeOperation.executeOperationsAccountWithdraw(scanner);
+        }
+        if (operation.equals("ACCOUNT_DEPOSIT")) {
+            executeOperation.executeOperationsAccountDeposit(scanner);
+        }
+        if (operation.equals("USER_CREATE")) {
+            executeOperation.executeOperationsUserCreate(scanner);
+        }
+        if (operation.equals("ACCOUNT_TRANSFER")) {
+            executeOperation.executeOperationsAccountTransfer(scanner);
         }
     }
 }
