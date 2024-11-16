e7192897e688e9756f62b7452f19964049bde09d
NoutVivo <fst8822@gmail.com>
2024-11-16 11:08:56 +0300

    refactor AccountDao

--- src/main/java/org/das/dao/AccountDao.java
+++ src/main/java/org/das/dao/AccountDao.java
@@ -3,7 +3,6 @@ package org.das.dao;
 import org.das.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
