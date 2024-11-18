59d6d964e2a6bf43e0d76a3468b6a12199aae08a
ПК_Home <wrofdt@ya.ru>
2024-11-18 22:19:15 +0300

    Create AccountProperties

--- src/main/java/org/das/utils/AccountProperties.java
+++ src/main/java/org/das/utils/AccountProperties.java
@@ -1,15 +1,13 @@
 package org.das.utils;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AccountProperties {
     private final int defaultAmount;
     private final double transferCommission;
 
-    public AccountProperties(@Value("${account.default-amount}") int defaultAmount,
-                             @Value("${account.transfer-commission}") double transferCommission) {
+    public AccountProperties( int defaultAmount,
+                              double transferCommission) {
         this.defaultAmount = defaultAmount;
         this.transferCommission = transferCommission;
     }
