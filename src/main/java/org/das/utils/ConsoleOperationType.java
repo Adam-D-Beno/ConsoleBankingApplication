811601bb8711bdb57f6d992024f3c3f13fa1f52c
ПК_Home <wrofdt@ya.ru>
2024-11-18 19:55:12 +0300

    refactor OperationsConsoleListener.java

---
+++ src/main/java/org/das/utils/ConsoleOperationType.java
@@ -0,0 +1,12 @@
+package org.das.utils;
+
+public class ConsoleOperationType {
+
+    ACCOUNT_CREATE,
+    SHOW_ALL_USERS,
+    ACCOUNT_CLOSE,
+    ACCOUNT_WITHDRAW,
+    ACCOUNT_DEPOSIT,
+    ACCOUNT_TRANSFER,
+    USER_CREATE;
+}
