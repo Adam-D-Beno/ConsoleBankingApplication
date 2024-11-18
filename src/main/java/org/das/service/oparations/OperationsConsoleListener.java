e09d10075d85fd9416b9229bd0d1b88730f9ed6b
ПК_Home <wrofdt@ya.ru>
2024-11-18 20:55:41 +0300

    Create OperationCommandConfiguration

--- src/main/java/org/das/service/oparations/OperationsConsoleListener.java
+++ src/main/java/org/das/service/oparations/OperationsConsoleListener.java
@@ -3,7 +3,6 @@ package org.das.service.oparations;
 import org.das.utils.ConsoleOperationType;
 import org.springframework.stereotype.Component;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class OperationsConsoleListener implements Runnable {
