244477ed1a53a70625271dde8c3408ae427d6dae
ПК_Home <wrofdt@ya.ru>
2024-11-18 22:20:06 +0300

    refactor OperationCommandConfig

--- src/main/java/org/das/config/OperationCommandConfig.java
+++ src/main/java/org/das/config/OperationCommandConfig.java
@@ -1,7 +1,10 @@
 package org.das.config;
 
+import org.das.service.AccountService;
 import org.das.service.ExecuteOperation;
+import org.das.service.UserService;
 import org.das.utils.ConsoleOperationType;
+import org.das.validate.UserValidation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,11 +17,23 @@ import java.util.function.Consumer;
 public class OperationCommandConfig {
 
     @Bean
-    public Map<ConsoleOperationType, Consumer<Scanner>> processorMap(Scanner scanner, ExecuteOperation executeOperation) {
-        var processorMap = new EnumMap<ConsoleOperationType, ExecuteOperation>(ConsoleOperationType.class);
-        processorMap.put(ConsoleOperationType.USER_CREATE, s -> executeOperation.executeOperationsUserCreate(s));
-//        processorMap.put(ConsoleOperationType.ACCOUNT_CREATE, createAccountProcessor);
-//        processorMap.put(ConsoleOperationType.SHOW_ALL_USERS, showAllUsersProcessor);
+    public Map<ConsoleOperationType, Consumer<Scanner>> processorMap(ExecuteOperation executeOperation) {
+        var processorMap = new EnumMap<ConsoleOperationType, Consumer<Scanner>>(ConsoleOperationType.class);
+        processorMap.put(ConsoleOperationType.USER_CREATE, executeOperation::executeOperationsUserCreate);
+        processorMap.put(ConsoleOperationType.ACCOUNT_CREATE, executeOperation::executeOperationsAccountCreate);
+        processorMap.put(ConsoleOperationType.SHOW_ALL_USERS, s -> executeOperation.showAllUsers());
+        processorMap.put(ConsoleOperationType.ACCOUNT_CLOSE, executeOperation::executeOperationsAccountClose);
+        processorMap.put(ConsoleOperationType.ACCOUNT_WITHDRAW, executeOperation::executeOperationsAccountWithdraw);
+        processorMap.put(ConsoleOperationType.ACCOUNT_DEPOSIT, executeOperation::executeOperationsAccountDeposit);
+        processorMap.put(ConsoleOperationType.ACCOUNT_TRANSFER, executeOperation::executeOperationsAccountTransfer);
         return processorMap;
+    }
+
+    @Bean
+    public ExecuteOperation executeOperation(UserValidation userValidation,
+                                             UserService userService,
+                                             AccountService accountService
+    ) {
+        return new ExecuteOperation(userValidation, userService, accountService);
     }
 }
