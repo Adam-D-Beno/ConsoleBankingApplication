a61db054ebd996302af696ce9264f01090d050f4
ПК_Home <wrofdt@ya.ru>
2024-11-22 21:23:21 +0300

    refactor AccountServiceImpl.java

--- src/main/java/org/das/service/AccountServiceImpl.java
+++ src/main/java/org/das/service/AccountServiceImpl.java
@@ -3,18 +3,13 @@ package org.das.service;
 import org.das.dao.AccountDao;
 import org.das.dao.UserDao;
 import org.das.model.Account;
-import org.das.model.User;
 import org.das.utils.AccountProperties;
 import org.das.validate.AccountValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class AccountServiceImpl implements AccountService {
@@ -35,21 +30,16 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public Account create(UUID userId) {
         Account newAccount = new Account(getRandomId(), userId);
-        setDefaultAmount(newAccount);
+       if (isFirstAccount(newAccount.getAccountId())) {
+           newAccount.setMoneyAmount(BigDecimal.valueOf(accountProperties.getDefaultAmount()));
+       }
         return accountDao.save(newAccount);
-    }
-
-    private void setDefaultAmount(Account account) {
-        if (isFirstAccount(account)) {
-            account.setMoneyAmount(BigDecimal.valueOf(accountProperties.getDefaultAmount()));
-        }
     }
 
     @Override
     public Account close(UUID accountId) {
-        Account account = accountDao.getAccount(accountId)
-                .orElseThrow(() -> new IllegalArgumentException("No such account: id=%s".formatted(accountId)));
-        if (hasNoAccounts(account)) {
+        Account account = getAccount(accountId);
+        if (isOnlyAccount(account.getUserId())) {
             throw new IllegalArgumentException(("Account with id=%s cant delete, " +
                     "because user have only one account").formatted(accountId));
         }
@@ -60,16 +50,14 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void deposit(UUID accountId, BigDecimal amount) {
         accountValidation.negativeAmount(amount);
-        Account account = accountDao.getAccount(accountId)
-                .orElseThrow(() -> new IllegalArgumentException("Account not exist id=%s".formatted(accountId)));
+        Account account = getAccount(accountId);
         account.increaseAmount(amount);
     }
 
     @Override
     public void withdraw(UUID accountId, BigDecimal amount) {
         accountValidation.negativeAmount(amount);
-        Account account = accountDao.getAccount(accountId)
-                .orElseThrow(() -> new IllegalArgumentException("Account not exist id=%s".formatted(accountId)));
+        Account account = getAccount(accountId);
         accountValidation.negativeBalance(account, amount);
         account.decreaseAmount(amount);
     }
@@ -77,12 +65,9 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public void transfer(UUID senderId, UUID recipientId, BigDecimal amount) {
         accountValidation.negativeAmount(amount);
-        Account fromAccount = accountDao.getAccount(senderId)
-                .orElseThrow(() -> new IllegalArgumentException("No such account: id=%s".formatted(senderId)));
-        accountValidation.negativeBalance(fromAccount, amount);
-        Account toAccount = accountDao.getAccount(recipientId)
-                .orElseThrow(() -> new IllegalArgumentException("No such account: id=%s".formatted(recipientId)));
-        ownAccountTransfer(fromAccount, toAccount);
+        Account fromAccount = getAccount(senderId);
+        Account toAccount = getAccount(recipientId);
+        accountValidation.validateDifferentAccounts(fromAccount, toAccount);
         BigDecimal amountAfterCommission = calculateAmountAfterCommission(amount);
         fromAccount.decreaseAmount(amount);
         toAccount.increaseAmount(amountAfterCommission);
@@ -90,29 +75,28 @@ public class AccountServiceImpl implements AccountService {
 
     private BigDecimal calculateAmountAfterCommission(BigDecimal amount) {
         BigDecimal commission = BigDecimal.valueOf(accountProperties.getTransferCommission());
-        BigDecimal multiplier = BigDecimal.ONE.subtract(commission);
-        return amount.multiply(multiplier).setScale(0, RoundingMode.HALF_UP);
+        return amount.multiply(BigDecimal.ONE.subtract(commission)).setScale(2, RoundingMode.HALF_UP);
     }
 
-    private boolean hasNoAccounts(Account account) {
-        return userDao.getUsers().stream()
-    .filter(user -> user.getUserId().equals(account.getUserId()))
-    .anyMatch(user -> user.getAccounts().isEmpty());
+    private boolean isOnlyAccount(UUID userId) {
+        return userDao.getUser(userId).stream()
+                .mapToInt(user -> user.getAccounts().size())
+                .anyMatch(count -> count <= 1);
+
     }
 
-    private boolean isFirstAccount(Account account) {
-        return hasNoAccounts(account);
-    }
-
-    private void ownAccountTransfer(Account fromAccount, Account toAccount) {
-        if (fromAccount.getAccountId().equals(toAccount.getAccountId())) {
-            throw new IllegalArgumentException("Account from id=%s and account to id=%s  transfer is same"
-                    .formatted(fromAccount.getAccountId(), toAccount.getAccountId()));
-        }
+    private boolean isFirstAccount(UUID userId) {
+        return isOnlyAccount(userId);
     }
 
     private UUID getRandomId() {
         return UUID.randomUUID();
+    }
+
+    private Account getAccount(UUID accountId) {
+      return accountDao.getAccount(accountId)
+                .orElseThrow(() -> new IllegalArgumentException("Account not exist id=%s".formatted(accountId)));
+
     }
 }
 
