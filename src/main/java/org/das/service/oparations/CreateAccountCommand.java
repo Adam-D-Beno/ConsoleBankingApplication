1924c763165b250317fb90b681d2d613d6aca9a9
ПК_Home <wrofdt@ya.ru>
2024-11-22 16:01:38 +0300

    refactor CreateAccountCommand.java

--- src/main/java/org/das/service/oparations/CreateAccountCommand.java
+++ src/main/java/org/das/service/oparations/CreateAccountCommand.java
@@ -1,12 +1,14 @@
 package org.das.service.oparations;
 
 import org.das.model.Account;
+import org.das.model.User;
 import org.das.service.AccountService;
 import org.das.service.UserService;
 import org.das.utils.ConsoleOperationType;
 import org.das.validate.UserValidation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -40,7 +42,9 @@ public class CreateAccountCommand implements OperationCommand {
         String userId = scanner.nextLine();
         userValidation.userLoginCorrect(userId);
         Account account = accountService.create(UUID.fromString(userId));
-        System.out.println("New account created with ID: " + account.getAccountId() +
-                userService.getUserById(account.getUserId()).get().getLogin());
+        User user = userService.getUserById(account.getUserId()).get();
+        user.addAccount(account);
+        System.out.println("New account created with ID =%s for user with id=%s"
+                .formatted(account.getAccountId(), user.getUserId()));
     }
 }
